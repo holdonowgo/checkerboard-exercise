a2d7f65b19df9da70f04a4466adbe29815277fec
Randall Spencer <randallspencer@gmail.com>
2017-01-22 19:34:16 -0800

    Finish checkerboard and random color.

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // Your JS goes here
 var body = document.querySelector('body');
-// var divRed = document.createElement('div');
-// var divBlack = document.createElement('div');
 
-// div.setAttribute('style', 'padding:11.1%');, 'width:11.1%, background-color:red, float:left');
-// Object.assign(divRed.style, {padding:"11.1%", width:'11.1', backgroundColor:"red", float:"left"});
-// Object.assign(divBlack.style, {padding:"11.1%", width:'11.1', backgroundColor:"black", float:"left"});
-// var gridDiv = document.createElement('div');
-// Object.assign(gridDiv.style, {
-for(let i = 0; i < 63; i++) {
-  if(i % 2 === 0) {
-    let divBlack = document.createElement('div');
-    Object.assign(divBlack.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:"black", display:'block', float:"left"});
-    body.appendChild(divBlack);
-  } else {
-    let divRed = document.createElement('div');
-    Object.assign(divRed.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:"red", display:'block', float:"left"});
-    body.appendChild(divRed);
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+function myFunction() {
+  for(let i = 0; i < 63; i++) {
+    if(i % 2 === 0) {
+      let divBlack = document.createElement('div');
+      Object.assign(divBlack.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"});
+      body.appendChild(divBlack);
+    } else {
+      let divRed = document.createElement('div');
+      Object.assign(divRed.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"});
+      body.appendChild(divRed);
+    }
   }
 }
+
+myFunction();
