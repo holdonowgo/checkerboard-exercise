cc69b12ef311b3cc681d3107629dde53d2d6e7cd
Randall Spencer <randallspencer@gmail.com>
2017-01-22 20:43:19 -0800

    Make "flashing" branch.

--- script.js
+++ script.js
@@ -2,26 +2,32 @@
 var body = document.querySelector('body');
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
 
-function myFunction() {
-  for(let i = 0; i < 63; i++) {
-    if(i % 2 === 0) {
-      let divBlack = document.createElement('div');
-      Object.assign(divBlack.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"});
-      body.appendChild(divBlack);
-    } else {
-      let divRed = document.createElement('div');
-      Object.assign(divRed.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"});
-      body.appendChild(divRed);
-    }
+function myFunction()
+{
+  for(let i = 0; i < 62; i++) {
+    let div = document.createElement('div');
+    Object.assign(div.style, {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"});
+    body.appendChild(div);
   }
+
+  setInterval(function()
+  {
+    let divs = body.children;
+    for(div of divs) {
+      Object.assign(
+        div.style,
+        {paddingBottom:"11.1%", width:'11.1%', backgroundColor:getRandomColor(), display:'block', float:"left"}
+      );
+    }
+  }, 2000);
 }
 
 myFunction();
